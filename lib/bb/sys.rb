953d9561bb1ea036e98968167c62aa5b80681799
Eric Zhang <really.ez@gmail.com>
2012-02-22 11:13:13 -0800

    support null callback in hook_channel_handler

--- lib/bb/sys.rb
+++ lib/bb/sys.rb
@@ -2,8 +2,11 @@ module Bridge
   # These are internal system functions, which should only be called by the
   # Erlang gateway.
   module Sys
-    def self.hook_channel_handler name, handler, fun
-      fun.call(Core::store(name, Core::lookup(handler), false), name)
+    def self.hook_channel_handler name, handler, fun = false
+      ref = Core::store(name, Core::lookup(handler), false)
+      if fun then
+        fun.call(ref, name)
+      end
     end
 
     def self.remote_error msg
