5d0333dca5fc48b482712c3d574ae8de97b86587
Steve Wang <steve.john.wang@gmail.com>
2012-02-22 15:49:48 -0800

    update docs, bump version

--- examples/pong/pong.rb
+++ examples/pong/pong.rb
@@ -1,26 +1,24 @@
 require 'eventmachine'
 require 'bridge'
 
-module EchoModule
-  def echo name, msg
-    puts(name + ": " + msg)
-  end
-  def pong msg, count, fun
-    if msg == "ping"
-      puts :pong
-      fun.call(count + 1)
-    end
-  end
-end
-
 EventMachine::run {
   Bridge::initialize({:reconnect => false, :port => 8090})
   Bridge::ready lambda {
     puts 'Connected.'
   }
   puts 'enqueued ready func'
-  Bridge::publish_service("pong", EchoModule)
+  Bridge::publish_service "echo", lambda {|*x| puts *x}
+  puts 'published echo service'
+  Bridge::publish_service "pong", lambda {|msg, count, fun|
+    if msg == "ping"
+      puts :pong
+      fun.call(count + 1)
+    end
+  }
   puts 'published pong service'
-  Bridge::join_channel("duuuude", EchoModule, lambda {puts "Duuude."})
+  Bridge::join_channel("duuuude",
+                       lambda {|name, msg|
+                         puts(name + ": " + msg)
+                       }, lambda {puts "Duuude."})
   puts 'joined channel'
 }
--- lib/bb/sys.rb
+++ lib/bb/sys.rb
@@ -2,11 +2,8 @@ module Bridge
   # These are internal system functions, which should only be called by the
   # Erlang gateway.
   module Sys
-    def self.hook_channel_handler name, handler, fun = false
-      ref = Core::store(name, Core::lookup(handler), false)
-      if fun
-        fun.call(ref, name)
-      end
+    def self.hook_channel_handler name, handler, fun
+      fun.call(Core::store(name, Core::lookup(handler), false))
     end
 
     def self.remote_error msg
