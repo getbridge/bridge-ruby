7092924427964331cc2c3c47f07e035710970f00
Steve Wang <steve.john.wang@gmail.com>
2012-02-22 20:02:59 -0800

    Fix example already.

--- examples/pong/pong.rb
+++ examples/pong/pong.rb
@@ -1,24 +1,27 @@
 require 'eventmachine'
 require 'bridge'
 
-EventMachine::run {
-  Bridge::initialize({:reconnect => false, :host => '127.0.0.1', :port => 8090})
-  Bridge::ready lambda {
-    puts 'Connected.'
-  }
-  puts 'enqueued ready func'
-  Bridge::publish_service "echo", lambda {|*x| puts *x}
-  puts 'published echo service'
-  Bridge::publish_service "pong", lambda {|msg, count, fun|
+
+module EchoModule
+  def self.echo name, msg
+    puts(name + ": " + msg)
+  end
+  def self.pong msg, count, fun
     if msg == "ping"
       puts :pong
       fun.call(count + 1)
     end
+  end
+end
+
+EventMachine::run {
+  Bridge::initialize({:reconnect => false, :port => 8090})
+  Bridge::ready lambda {
+    puts 'Connected.'
   }
+  puts 'enqueued ready func'
+  Bridge::publish_service("pong", EchoModule)
   puts 'published pong service'
-  Bridge::join_channel("duuuude",
-                       lambda {|name, msg|
-                         puts(name + ": " + msg)
-                       }, lambda {puts "Duuude."})
+  Bridge::join_channel("duuuude", EchoModule, lambda {puts "Duuude."})
   puts 'joined channel'
 }
