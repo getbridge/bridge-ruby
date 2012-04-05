63e261b60c9fa1d2f2ddde5e091125c6924252f6
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-04-05 13:28:43 -0700

    fixed chat example to be more standard

--- examples/chat/chat_client.rb
+++ examples/chat/chat_client.rb
@@ -1,21 +1,21 @@
-require 'bridge'
+    require 'bridge'
 
-EventMachine.run do
-  
-  module ChatHandler
-  
-    def self.msg name, msg
-      puts name + ': ' + msg
+    EventMachine.run do
+
+      bridge = Bridge::Bridge.new(:api_key => 'abcdefgh', :log => 5)
+
+      class ChatHandler
+        def message sender, msg
+          print sender, ":", msg
+        end
+      end
+
+      bridge.connect
+
+      auth = bridge.get_service('auth')
+      auth.join('flotype-lovers', 'secret123', ChatHandler.new) do |channel, name|
+        print "Joined: ", name
+      end
+
     end
-      
-  end
-  
-  
-  bridge = Bridge::Bridge.new(:api_key => 'abcdefgh').connect
-  
-  bridge.join_channel('lobby', ChatHandler)
-  
-  
-end
-
 
