4f3393393e2d6d03125d06d0f7d2b4f0135eb177
Sridatta Thatipamala <sthatipamala@gmail.com>
2012-06-06 19:01:39 -0700

    add openssl support to Tcp

--- lib/tcp.rb
+++ lib/tcp.rb
@@ -1,8 +1,9 @@
 require 'eventmachine'
+require 'openssl'
 
 module Bridge
   class Tcp < EventMachine::Connection #:nodoc: all
-  
+
     def initialize connection
       @buffer = ''
       @len = 0
@@ -11,9 +12,22 @@ module Bridge
       @connection = connection
       start
     end
-    
+
     def post_init
+      start_tls(:verify_peer => true) if @connection.options[:secure]
+    end
+
+    def connection_completed
+      # connection now ready. call the callback
       @connection.onopen self
+    end
+
+    def ssl_verify_peer cert
+        cert = OpenSSL::X509::Certificate.new(cert)
+        ca_cert = OpenSSL::X509::Certificate.new(File.read('/Users/sridatta/flotype.crt'))
+        puts cert.inspect
+        puts cert.verify(ca_cert.public_key)
+        true
     end
 
     def receive_data data
@@ -27,14 +41,14 @@ module Bridge
         @pos = @pos + data.length
       end
     end
-    
+
     def read len, &cb
       @buffer = ''
       @len = len
       @pos = 0
       @callback = cb
     end
-    
+
     def start
       # Read header bytes
       read 4 do |data|
@@ -52,10 +66,10 @@ module Bridge
       # Prepend length header to message
       send_data([arg.length].pack("N") + arg)
     end
-    
+
     def unbind
       @connection.onclose
     end
-    
-  end  
+
+  end
 end
